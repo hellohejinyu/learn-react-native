0f621baf0c1dbbc179e9888b6176346b2306c4af
hekaijun <cadenho@hotmail.com>
2019-08-02 14:18:14 +0800

    feat: Image

--- App.tsx
+++ App.tsx
@@ -21,89 +21,18 @@ import {
   Image
 } from 'react-native';
 
-const styles = StyleSheet.create({
-  btnWrappper: {
-    width: '50%',
-  }
-})
 
 const App = () => {
-  const listRef = useRef() as any
-  const drawerRef = useRef() as any
-  const getRandomList = useCallback(() => {
-    return new Array(100).fill(0).map(() => ({ key: Math.floor(Math.random() * 100000).toString() }))
-  }, [])
-  const navigationView = useMemo(() => (
-    <View style={{flex: 1, backgroundColor: '#fff'}}>
-      <Text style={{margin: 10, fontSize: 15, textAlign: 'left'}}>I'm in the Drawer!</Text>
-      <Button title='close' onPress={() => {
-        drawerRef.current.closeDrawer()
-      }} />
-    </View>
-  ), [])
-  const [list, setList] = useState(getRandomList())
-  const [disabled, setDisabled] = useState(false)
-  const [refreshing, setRefreshing] = useState(false)
   return (
-    <DrawerLayoutAndroid
-      ref={drawerRef}
-      drawerWidth={300}
-      drawerPosition={DrawerLayoutAndroid.positions.Right}
-      renderNavigationView={() => navigationView}>
-      <View
-        style={{ display: 'flex', flex: 1 }}
-      >
-        <FlatList
-          ref={listRef}
-          style={{ flexGrow: 1 }}
-          ItemSeparatorComponent={({highlighted}) => (
-            <View style={[highlighted && {marginLeft: 0, height: 10, backgroundColor: 'red', width: '100%'}]} />
-          )}
-          refreshing={refreshing}
-          onRefresh={() => {
-            setRefreshing(true)
-            setTimeout(() => {
-              setList(getRandomList())
-              setRefreshing(false)
-            }, 2000);
-          }}
-          data={list}
-          extraData={disabled}
-          renderItem={({item, index, separators}) => (
-            <TouchableHighlight
-              onPress={() => {}}
-              onShowUnderlay={separators.highlight}
-              onHideUnderlay={separators.unhighlight}
-            >
-              <Text style={disabled && {color: 'red'}}>{item.key}</Text>
-            </TouchableHighlight>
-          )}
-          onEndReached={(info) => {
-            console.log(info)
-          }}
-          progressViewOffset={50}
-          onEndReachedThreshold={0.1}
-        />
-        <Button
-          title='open open'
-          onPress={() => {
-            drawerRef.current.openDrawer()
-          }}
-        />
-        <Button
-          title='toggle disabled'
-          onPress={() => {
-            setDisabled(!disabled)
-          }}
-        />
-        <Button
-          title='scroll to end'
-          onPress={() => {
-            listRef.current.scrollToEnd()
-          }}
-        />
-      </View>
-    </DrawerLayoutAndroid>
+    <Image
+      source={{uri:'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=144388917,3393541021&fm=26&gp=0.jpg'}}
+      style={{ 
+        width: 160,
+        height: 90,
+        borderRadius: 15,
+        overlayColor: 'red'
+      }}
+    />
   );
 };
 
