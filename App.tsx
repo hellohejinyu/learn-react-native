de1adf0b4ef5dc02b1e5c5eb598660062b280940
hekaijun <cadenho@hotmail.com>
2019-08-04 21:19:57 +0800

    feat: ProgressBarAndroid

--- App.tsx
+++ App.tsx
@@ -12,53 +12,35 @@ import React, { useCallback, useRef, useMemo, useState } from 'react';
 import {
   Button,
   Text,
+  ProgressBarAndroid,
+  StyleSheet,
   View,
   Modal,
+  Picker
 } from 'react-native';
 
 
 const App = () => {
-  const [v, setV] = useState(false)
   return (
-    <View>
-      <Modal
-        animationType='slide'
-        visible={v}
-        transparent={true}
-        onDismiss={() => {
-          console.log('dismiss')
-        }}
-        onRequestClose={() => {
-          console.log('requestClose')
-          setV(false)
-        }}
-      >
-        <View
-          style={{
-            backgroundColor: 'rgba(0, 0, 0, .6)',
-            padding: 16,
-            height: 300,
-            width: 200,
-            marginTop: 50
-          }}
-        >
-          <Text>Hello World</Text>
-          <Button
-            title='close'
-            onPress={() => {
-              setV(false)
-            }}
-          />
-        </View>
-      </Modal>
-      <Button
-        title='show'
-        onPress={() => {
-          setV(true)
-        }}
+    <View style={styles.container}>
+      <ProgressBarAndroid />
+      <ProgressBarAndroid styleAttr="Horizontal" />
+      <ProgressBarAndroid styleAttr="Horizontal" color="#2196F3" />
+      <ProgressBarAndroid
+        styleAttr="Horizontal"
+        indeterminate={false}
+        progress={0.5}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-evenly',
+    padding: 10,
+  },
+});
 
 export default App;
